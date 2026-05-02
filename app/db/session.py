670915fcf46b1215fd41ba34e204fba85bf97537
Palle Sandhya <sandhya.yourmail@gmail.com>
2026-05-02 12:59:07 +0530

    verify otp

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.db.base import Base
+from app.core.config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Sandhya1997@localhost:5433/nhrc"
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
+)
 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 def get_db():
     db = SessionLocal()
