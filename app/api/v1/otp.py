70771d149179eba7ae85a90465ca5aa2ff3e28e6
Palle Sandhya <sandhya.yourmail@gmail.com>
2026-05-02 12:04:27 +0530

    verify otp

--- app/api/v1/otp.py
+++ app/api/v1/otp.py
@@ -4,7 +4,7 @@ from app.core.database import get_db
 from app.schemas.otp import OTPRequest, OTPVerifyRequest
 from app.services.otp_service import OTPService
 from app.utils.email_sender import EmailService
-from app.services.otp_service import verify_otp
+from app.services.otp_service import OTPService
 
 
 router = APIRouter(prefix="/otp", tags=["OTP"])
@@ -17,8 +17,9 @@ def send_otp(email: str):
 
 
 @router.post("/verify")
-def verify_otp_api(payload: OTPVerifyRequest):
-    is_valid, message = verify_otp(payload.email, payload.otp)
+def verify_otp(payload: OTPVerifyRequest):
+    #is_valid, message = verify_otp(payload.email, payload.otp)
+    is_valid, message = OTPService.verify_otp(payload.email, payload.otp)
 
     if not is_valid:
         raise HTTPException(status_code=400, detail=message)
